5509a50b4e7821022ca586ff945f88145b848e63
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2023-03-20 13:21:11 +0100

    Refactored projects links

---
+++ src/Projects.js
@@ -0,0 +1,34 @@
+export const PROJECTS = [
+  {
+    title: 'E-commerce product page',
+    frontEndMentorUrl: 'https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6',
+    projectUrl: 'ecommerce-product-page',
+    img: 'ecommerce.png',
+  },
+  {
+    title: 'Tip calculator app',
+    frontEndMentorUrl: 'https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX',
+    projectUrl: 'tip-calculator-app',
+    img: 'tip-calculator.png',
+  },
+  {
+    title: 'Results summary component',
+    frontEndMentorUrl:
+      'https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV',
+    projectUrl: 'results-summary-component',
+    img: 'results-summary.png',
+  },
+  {
+    title: 'Pricing component with toggle',
+    frontEndMentorUrl:
+      'https://www.frontendmentor.io/challenges/pricing-component-with-toggle-8vPwRMIC',
+    projectUrl: 'pricing-component',
+    img: 'pricing-component.png',
+  },
+  {
+    title: 'News homepage',
+    frontEndMentorUrl: 'https://www.frontendmentor.io/challenges/news-homepage-H6SWTa1MFl',
+    projectUrl: 'news-homepage',
+    img: 'news-homepage.png',
+  },
+];
